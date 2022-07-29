7a29d5181eef8db5810d9230efc960751e74ea53
Mason Stuart <mason.stuart05@outlook.com>
2022-07-29 15:13:19 -0700

    Tutor Sort

---
+++ public/js/dash.js
@@ -0,0 +1,15 @@
+function logOut() {
+    firebase.auth().signOut()
+        .then(function () {
+            window.location.href = '../index.html'
+        })
+        .catch(function (error) {
+
+        });
+}
+
+function printUsers() {
+    firebase.database().ref("users").orderByChild("accountType").equalTo("tutor").on("child_added", (snapshot) => {
+        console.log(snapshot.val());
+    });
+}
