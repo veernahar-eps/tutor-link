b2375f09715bc5f84490d2aed89ccb458627a5d7
veernahar <43395575+veernahar@users.noreply.github.com>
2022-07-29 13:11:56 -0700

    fixed checkUserExists()

--- public/js/home.js
+++ public/js/home.js
@@ -1,68 +1,30 @@
 const key = window.localStorage.key(1)
 const data = window.localStorage.getItem(key)
 
-if (data && data.user) {
-    checkUserOnDatabase(data.user);
+if (data.user) {
+    checkUserExists(data.user).then(r => r);
 } else {
     document.getElementById("profile_nav").style.display = 'none'
 }
 
-async function checkUserOnDatabase(user) {
-    
-    await firebase.database().ref(`users/tutors/${user.uid}`).once("value").then(snapshot => {
-        if (data && !snapshot.exists()) {
-            async function log(){await firebase.database().ref(`users/students/${user.uid}`).once("value").then(snapshot => {
-                if(!snapshot.exists){
-                    logOut();
-                }
-            });}
-            log();
-        }
-
-        if (data && snapshot.exists()) {
+async function checkUserExists(user) {
+    await firebase.database().ref(`users/${user.uid}`).once("value").then(snapshot => {
+        if (!snapshot.exists()) {
+            logOut();
+        } else {
             const json = JSON.parse(data)
-        
+
             const displayName = json['displayName']
             const photoURL = json['photoURL']
-        
+
             console.log(displayName)
             console.log(photoURL)
-        
+
             document.getElementById("student_login").style.display = 'none'
             document.getElementById("tutor_login").style.display = 'none'
             document.getElementById("profile_pic").src = photoURL
             document.getElementById("display_name").innerText = displayName
-        
-        } else {
-            document.getElementById("profile_nav").style.display = 'none'
-        }
-    });
 
-    await firebase.database().ref(`users/students/${user.uid}`).once("value").then(snapshot => {
-        if (data && !snapshot.exists()) {
-            async function log(){await firebase.database().ref(`users/tutors/${user.uid}`).once("value").then(snapshot => {
-                if(!snapshot.exists){
-                    logOut();
-                }
-            });}
-            log();
-        }
-        if (data && snapshot.exists()) {
-            const json = JSON.parse(data)
-        
-            const displayName = json['displayName']
-            const photoURL = json['photoURL']
-        
-            console.log(displayName)
-            console.log(photoURL)
-        
-            document.getElementById("student_login").style.display = 'none'
-            document.getElementById("tutor_login").style.display = 'none'
-            document.getElementById("profile_pic").src = photoURL
-            document.getElementById("display_name").innerText = displayName
-        
-        } else {
-            document.getElementById("profile_nav").style.display = 'none'
         }
     });
 }
