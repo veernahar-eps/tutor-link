e4305d004e69265c917f82fe495a2ce63ddaa950
veernahar <43395575+veernahar@users.noreply.github.com>
2022-07-30 16:24:30 -0700

    eE

--- public/js/dash.js
+++ public/js/dash.js
@@ -8,12 +8,19 @@ function logOut() {
         });
 }
 
-firebase.auth().onStateChanged(function (user) {
-    if (user) {
-        console.log(user.uid);
-        // return user data
-    } else {
-        console.log("user signed out");
-        // redirect to homepage?
-    }
-})
+function printUsers() {
+    firebase.database().ref("users").orderByChild("accountType").equalTo("tutor").on("child_added", (snapshot) => {
+        console.log(snapshot.val()['userData']['displayName']);
+    });
+}
+
+const key = window.localStorage.key(1)
+const data = window.localStorage.getItem(key)
+
+const json = JSON.parse(data)
+
+const displayName = json['displayName']
+const photoURL = json['photoURL']
+
+document.getElementById("profile_pic").src = photoURL
+document.getElementById("display_name").innerText = displayName
