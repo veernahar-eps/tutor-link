1c09f2130baa852f789d651676599d28c110d164
Mason Stuart <mason.stuart05@outlook.com>
2022-07-30 17:08:26 -0700

    Modified bio page

--- public/js/dash.js
+++ public/js/dash.js
@@ -15,14 +15,29 @@ const json = JSON.parse(data)
 
 const displayName = json['displayName']
 const photoURL = json['photoURL']
-const bio = json['bio']
-const arr = json['displayName'].split(" ")
-const firstName = arr[0]
-const lastName = arr[1]
-const email = json['email']
-const phone = json['phone']
-const state = json['state']
-const country = json['country']
+
+let displayNameArr = json['displayName'].split(" ")
+const firstName = displayNameArr[0]
+const lastName = displayNameArr[1]
+
+const curUID = json['uid']
+// get snapshot of current staet
+// used uid to grab each peice
+// snapshot.val().userData.email
+firebase.database().ref('users/' + curUID).on('child_added', (snapshot) => {
+    const bio = snapshot.val()['bio']
+    const email = snapshot.val()['email']
+    const state = snapshot.val()['state']
+    const country = snapshot.val()['country']
+    const phoneNumber = snapshot.val()['phone']
+});
 
 document.getElementById("profile_pic").src = photoURL
 document.getElementById("display_name").innerText = displayName
+document.getElementById("display_email").innerText = email
+document.getElementById("first_name")[0].placeholder = firstName
+document.getElementById("last_name")[0].placeholder = lastName
+document.getElementById("phone_number")[0].placeholder = phoneNumber
+document.getElementById("email")[0].placeholder = email
+document.getElementById("country")[0].placeholder = country
+document.getElementById("state")[0].placeholder = state
